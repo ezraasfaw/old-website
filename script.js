c78aef6e909d413043543066de5b378999638579
Ezra Asfaw <65423750+ezraasfaw@users.noreply.github.com>
2023-12-28 12:04:43 -0500

    worked on animation and added dark mode

---
+++ script.js
@@ -0,0 +1,104 @@
+var cursor = document.getElementById('cursor');
+
+document.addEventListener('mousemove', function(e) {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+});
+
+const ul = document.querySelector('ul');
+
+if (ul) {
+  ul.addEventListener('mouseover', () => {
+    cursor.style.backgroundColor = 'red';
+  });
+
+  ul.addEventListener('mouseout', () => {
+    cursor.style.backgroundColor = 'transparent';
+  });
+}
+
+
+
+
+const back = document.querySelector('.back-button');
+
+back.addEventListener('mouseover', () => {
+  cursor.style.backgroundColor = 'red';
+});
+
+back.addEventListener('mouseout', () => {
+  cursor.style.backgroundColor = 'white';
+  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+});
+
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('mouseover', () => {
+  cursor.style.backgroundColor = 'red';
+});
+
+menuButton.addEventListener('mouseout', () => {
+  cursor.style.backgroundColor = 'white';
+  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+});
+
+
+
+
+
+
+// Get references to the elements
+const darkModeToggle = document.getElementById('darkModeToggle');
+const logoImage = document.getElementById('logoImage');
+
+// Function to set dark mode
+function setDarkMode(isDarkMode) {
+  document.body.classList.toggle('dark-mode', isDarkMode);
+  darkModeToggle.textContent = isDarkMode ? 'LIGHT MODE' : 'DARK MODE';
+  logoImage.src = isDarkMode ? 'assets/logo_W.png' : 'assets/logo_B.png';
+  darkModeToggle.classList.toggle('light-mode', isDarkMode);
+  localStorage.setItem('darkMode', isDarkMode ? 'enabled' : null);
+}
+
+// Check if dark mode was previously set
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+setDarkMode(isDarkMode);
+
+// Add event listener for the toggle button
+darkModeToggle.addEventListener('click', () => {
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  setDarkMode(!isDarkMode);
+});
+
+
+
+
+const text = document.querySelector('.text');
+const menuOptions = document.getElementById('menuOptions');
+const nav = document.querySelector('.nav_content'); 
+
+nav.addEventListener('mouseover', () => {
+  cursor.style.backgroundColor = 'red';
+});
+
+nav.addEventListener('mouseout', () => {
+  cursor.style.backgroundColor = 'white';
+  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+});
+
+
+menuButton.addEventListener('click', () => {
+  // If menuOptions is displayed, hide it, otherwise, display it
+  if (menuOptions.style.display === 'block') {
+    location.reload(); // Refresh the current page
+  } else {
+    menuButton.textContent = 'RETURN';
+    text.innerHTML = ''; // Clear out the text after the animation
+    nav.innerHTML = ''; // Hide the nav-content
+    menuOptions.style.display = 'block'; // Display the navigation options
+    nav.style.display = 'block'; // Show the nav-content
+  }
+});
+
+
