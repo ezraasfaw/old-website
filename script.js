186f0c10e3a96f675dffb1eb947dc722bddf7022
Ezra Asfaw <65423750+ezraasfaw@users.noreply.github.com>
2023-12-29 19:30:46 -0500

    forgot to uncomment a line in style.css and did some cleaning in script.js

--- script.js
+++ script.js
@@ -1,72 +1,81 @@
+// Get references to the elements
+const back = document.querySelector('.back-button');
+const menuButton = document.querySelector('.menu-button');
+const darkModeToggle = document.getElementById('darkModeToggle');
+const logoImage = document.getElementById('logoImage');
+const menuOptions = document.getElementById('menuOptions');
+const nav = document.querySelector('.nav_content');
+const textDiv = document.querySelector('.text');
+const typewriter = document.querySelector('.typewriter');
+const backButton = document.querySelector('.back-button');
+const text = document.querySelector('.text'); 
+
+
+// Wait for the window to load before executing the script
 window.onload = function() {
-  
+  // Get the cursor element
   var cursor = document.getElementById('cursor');
 
-// For mouse movement
-document.addEventListener('mousemove', function(e) {
-  cursor.style.left = e.pageX + 'px';
-  cursor.style.top = e.pageY + 'px';
-},false);
+  // Event listener for mouse movement
+  document.addEventListener('mousemove', function(e) {
+    // Update the cursor's position to follow the mouse
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+  }, false);
 
-// For touch movement
-document.addEventListener('touchmove', function(e) {
-  // Prevent the browser from doing its default thing (scroll, zoom)
-  e.preventDefault();
+  // Event listener for touch movement
+  document.addEventListener('touchmove', function(e) {
+    // Prevent the browser from doing its default thing (scroll, zoom)
+    e.preventDefault();
 
-  var touch = e.touches[0]; // Get the information for finger #1
+    // Get the information for finger #1
+    var touch = e.touches[0];
 
-  cursor.style.left = touch.pageX + 'px';
-  cursor.style.top = touch.pageY + 'px';
-}, { passive: false }); // Add this line
+    // Update the cursor's position to follow the touch
+    cursor.style.left = touch.pageX + 'px';
+    cursor.style.top = touch.pageY + 'px';
+  }, { passive: false }); // This line tells the browser that the event handler may call preventDefault()
 
-const ul = document.querySelector('ul');
+  // Get the ul element
+  const ul = document.querySelector('ul');
 
-if (ul) {
-  ul.addEventListener('mouseover', () => {
+  // If the ul element exists, add event listeners for mouseover and mouseout
+  if (ul) {
+    ul.addEventListener('mouseover', () => {
+      cursor.style.backgroundColor = 'red';
+    });
+
+    ul.addEventListener('mouseout', () => {
+      cursor.style.backgroundColor = 'transparent';
+    });
+  }
+
+  // Get the back button element
+  const back = document.querySelector('.back-button');
+
+  // Add event listeners for mouseover and mouseout to the back button
+  back.addEventListener('mouseover', () => {
     cursor.style.backgroundColor = 'red';
   });
 
-  ul.addEventListener('mouseout', () => {
-    cursor.style.backgroundColor = 'transparent';
+  back.addEventListener('mouseout', () => {
+    cursor.style.backgroundColor = 'white';
+    cursor.style.mixBlendMode = "difference"; // This will invert the color
   });
 }
 
 
-
-
-const back = document.querySelector('.back-button');
-
-back.addEventListener('mouseover', () => {
-  cursor.style.backgroundColor = 'red';
-});
-
-back.addEventListener('mouseout', () => {
-  cursor.style.backgroundColor = 'white';
-  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
-});
-
-
-const menuButton = document.querySelector('.menu-button');
-
+// Add event listeners for mouseover and mouseout to the menu button
 menuButton.addEventListener('mouseover', () => {
   cursor.style.backgroundColor = 'red';
 });
 
 menuButton.addEventListener('mouseout', () => {
   cursor.style.backgroundColor = 'white';
-  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+  cursor.style.mixBlendMode = "difference"; // This will invert the color
 });
 
 
-
-// Get references to the elements
-const darkModeToggle = document.getElementById('darkModeToggle');
-const logoImage = document.getElementById('logoImage');
-const menuOptions = document.getElementById('menuOptions'); // Add this line
-const nav = document.querySelector('.nav_content');
-const textDiv = document.querySelector('.text'); // Add this line
-const typewriter = document.querySelector('.typewriter');
-const backButton = document.querySelector('.back-button');
 
 // Function to set dark mode
 function setDarkMode(isDarkMode) {
@@ -94,29 +103,27 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 
-
-
-const text = document.querySelector('.text'); 
-
+// Add event listeners for mouseover and mouseout to the nav
 nav.addEventListener('mouseover', () => {
   cursor.style.backgroundColor = 'red';
 });
 
 nav.addEventListener('mouseout', () => {
   cursor.style.backgroundColor = 'white';
-  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+  cursor.style.mixBlendMode = "difference"; // This will invert the color
 });
 
+// Add event listeners for mouseover and mouseout to the menu options
 menuOptions.addEventListener('mouseover', () => {
   cursor.style.backgroundColor = 'red';
 });
 
 menuOptions.addEventListener('mouseout', () => {
   cursor.style.backgroundColor = 'white';
-  cursor.style.mixBlendMode = "difference"; /* This will invert the color */
+  cursor.style.mixBlendMode = "difference"; // This will invert the color
 });
 
-
+// Add event listener for click to the menu button
 menuButton.addEventListener('click', () => {
   // If menuOptions is displayed, hide it, otherwise, display it
   if (menuOptions.style.display === 'block') {
@@ -129,4 +136,3 @@ menuButton.addEventListener('click', () => {
     nav.style.display = 'block'; // Show the nav-content
   }
 });
-};
